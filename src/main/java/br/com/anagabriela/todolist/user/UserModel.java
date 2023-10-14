16bff90b0f43db818312002997071ee5f15768cf
Ana Gabriela <anagabrieladev@gmil.com>
2023-10-14 13:54:00 -0300

    🎉 first commit

---
+++ src/main/java/br/com/anagabriela/todolist/user/UserModel.java
@@ -0,0 +1,15 @@
+package br.com.anagabriela.todolist.user;
+
+public class UserModel {
+    private String name;
+    private String username;
+    private String password;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
