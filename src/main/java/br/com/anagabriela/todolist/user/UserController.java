bc82126e1b02aa014121eb97633a9236afdfca08
Ana Gabriela <anagabrieladev@gmil.com>
2023-10-14 15:35:32 -0300

    ✨ feat: return correct status code

--- src/main/java/br/com/anagabriela/todolist/user/UserController.java
+++ src/main/java/br/com/anagabriela/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.anagabriela.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +15,14 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         UserModel user = this.userRepository.findByUsername(userModel.getUsername());
         if (user != null) {
-            System.out.println("cadastrado");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists!");
         }
 
         UserModel userCreated = this.userRepository.save(userModel);
 
-        return userCreated;
+        return ResponseEntity.status(201).body(userCreated);
     }
 }
