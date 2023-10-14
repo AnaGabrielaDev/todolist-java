b46de943ecf812a6b54fc5722410f812f0f9a5eb
Ana Gabriela <anagabrieladev@gmil.com>
2023-10-14 13:59:11 -0300

    ➕ chore: add lombok to project

--- src/main/java/br/com/anagabriela/todolist/user/UserModel.java
+++ src/main/java/br/com/anagabriela/todolist/user/UserModel.java
@@ -1,15 +1,10 @@
 package br.com.anagabriela.todolist.user;
 
+import lombok.Data;
+
+@Data // lombok add getters and setter to Model
 public class UserModel {
     private String name;
     private String username;
     private String password;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
