1a0eec1d514c4ac6a3c45817e0fa77eed4c51c40
Ana Gabriela <anagabrieladev@gmil.com>
2023-10-14 15:18:44 -0300

    ➕ chore: add  database local

--- src/main/java/br/com/anagabriela/todolist/user/UserModel.java
+++ src/main/java/br/com/anagabriela/todolist/user/UserModel.java
@@ -1,10 +1,24 @@
 package br.com.anagabriela.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity(name = "users") // entidade cria no banco de dados
 @Data // lombok add getters and setter to Model
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     private String name;
     private String username;
     private String password;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
