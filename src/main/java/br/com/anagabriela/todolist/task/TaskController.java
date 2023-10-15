a95db3c971ef45271434783a6456c6f37fd4f11d
Ana Gabriela <anagabrieladev@gmil.com>
2023-10-15 18:47:37 -0300

    ✨ feat: edit only properties sended

--- src/main/java/br/com/anagabriela/todolist/task/TaskController.java
+++ src/main/java/br/com/anagabriela/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.anagabriela.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -47,8 +48,10 @@ public class TaskController {
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var userId = request.getAttribute("userId");
-        taskModel.setUserId((UUID) userId);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
