1a8c72cb7503e2f8c4b06c78a537f06e11b0a495
Ana Gabriela <anagabrieladev@gmil.com>
2023-10-15 19:08:00 -0300

    feat: throw exception if title lengh is bigger than 50

--- src/main/java/br/com/anagabriela/todolist/task/TaskModel.java
+++ src/main/java/br/com/anagabriela/todolist/task/TaskModel.java
@@ -34,4 +34,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("title length should be less than 50");
+        }
+        this.title = title;
+    }
 }
